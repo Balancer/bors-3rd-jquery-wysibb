8a848752ea1cc43e3d8170829ab476207c5adc02
Balancer <balancer@balancer.ru>
2014-05-07 05:05:08 +0400

    Init

---
+++ classes/wysibb.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+	Использование WYSIWYG-редактора BB-кода WysiBB
+	http://www.wysibb.com/
+*/
+
+class wysibb
+{
+	static function init($element, $attrs = array())
+	{
+		jquery_plugin::load('wysibb');
+		$attrs = blib_json::encode_jsfunc($attrs);
+		jquery::on_ready("\$('$element').wysibb($attrs);");
+		$theme = config('bower.path')."/jqjquery-wysibb/theme/default/wbbtheme.css";
+//		$theme = http://cdn.wysibb.com/css/default/wbbtheme.css;
+		bors_page::add_template_data_array('head_append', "<link rel=\"stylesheet\" href=\"{$theme}\" />");
+	}
+}
