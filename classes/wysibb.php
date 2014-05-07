aff8dbdca58cc661bbbca2167cfd68c2c9979c4a
Balancer <balancer@balancer.ru>
2014-05-07 06:48:37 +0400

    Works

--- classes/wysibb.php
+++ classes/wysibb.php
@@ -9,18 +9,27 @@ class wysibb
 {
 	static function init($element, $attrs = array())
 	{
-		jquery_plugin::load('wysibb');
+		jquery::load();
+
+//		jquery_plugin::assetic_load('wysibb');
+
+//		bors_use($path.css);
+//		jquery::plugin($path);
+//		http://cdn.wysibb.com/
+
+		bors_use('http://cdn.wysibb.com/js/jquery.wysibb.min.js');
+		bors_use('http://cdn.wysibb.com/css/default/wbbtheme.css');
+		bors_use('http://cdn.wysibb.com/lang/ru.ru');
+
 		$attrs = blib_json::encode_jsfunc($attrs);
 		jquery::on_ready("\$('$element').wysibb($attrs);");
-		$theme = config('bower.path')."/jqjquery-wysibb/theme/default/wbbtheme.css";
+//		$theme = config('bower.path')."/jqjquery-wysibb/theme/default/wbbtheme.css";
 //		$theme = http://cdn.wysibb.com/css/default/wbbtheme.css;
-		bors_page::add_template_data_array('head_append', "<link rel=\"stylesheet\" href=\"{$theme}\" />");
+//		bors_page::add_template_data_array('head_append', "<link rel=\"stylesheet\" href=\"{$theme}\" />");
 	}
 
 	static function __dev()
 	{
 		require '../../../autoload.php';
-		jquery_plugin::load('wysibb');
-		var_dump();
 	}
 }
